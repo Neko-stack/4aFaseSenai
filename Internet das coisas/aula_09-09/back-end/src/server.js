c6e0cf739604e9fbc23ee4e699ddd1d336da775d
GABRIEL LESSA COELHO <gabriel_l_coelho@estudante.sesisenai.org.br>
2026-09-09 21:42:52 -0300

    correção final

--- Internet das coisas/aula_09-09/back-end/src/server.js
+++ Internet das coisas/aula_09-09/back-end/src/server.js
@@ -16,6 +16,11 @@ app.get("/comentarios", (req, res) => {
 app.post("/comentarios", (req, res) => {
     const { comentario } = req.body;
 
+    // Validação básica se o comentário foi enviado
+    if (!comentario || typeof comentario !== "string") {
+        return res.status(400).json({ erro: "Comentário inválido." });
+    }
+    
     comentarios.push(comentario);
 
     res.json({
